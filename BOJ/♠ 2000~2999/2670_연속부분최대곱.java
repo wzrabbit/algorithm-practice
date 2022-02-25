9dd12d663220ef0ad6d56a8a4f99076d0e9fb9cf
wzrabbit <uicheonkim@gmail.com>
2022-02-25 13:13:59 +0900

    포맷팅

--- BOJ/♠ 2000~2999/2670_연속부분최대곱.java
+++ BOJ/♠ 2000~2999/2670_연속부분최대곱.java
@@ -2,19 +2,22 @@ import java.util.*;
 
 public class Main {
     public static void main(String args[]) {
-      Scanner sc = new Scanner(System.in);
-      int tc = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
 
-      double max = 0;
-      double current = 1;
+        double max = 0;
+        double current = 1;
 
-      for (int i = 0; i < tc; i++) {
-          double data = sc.nextDouble();
-          if (current >= 1) current *= data;
-          else current = data;
-          if (current > max) max = current;
-      }
-      
-      System.out.println(String.format("%.3f", max));
+        for (int i = 0; i < tc; i++) {
+            double data = sc.nextDouble();
+            if (current >= 1)
+                current *= data;
+            else
+                current = data;
+            if (current > max)
+                max = current;
+        }
+
+        System.out.println(String.format("%.3f", max));
     }
 }
